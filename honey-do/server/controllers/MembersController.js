32508db45a5701fde70d7687d94144fc6bcee628
Derek <derekbelloni10@gmail.com>
2022-03-24 12:24:00 -0600

    commit

--- honey-do/server/controllers/MembersController.js
+++
@@ -1,9 +0,0 @@
-import BaseController from "../utils/BaseController";
-
-export class CollaboratorsController extends BaseController {
-  constructor() {
-    super('api/members')
-    this.router
-
-  }
-}
