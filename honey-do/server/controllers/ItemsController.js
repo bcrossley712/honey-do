9eec009da10c6651fd39f3a1b427fa0d2523968e
Derek <derekbelloni10@gmail.com>
2022-03-24 10:14:39 -0600

    commit

--- honey-do/server/controllers/ItemsController.js
+++ honey-do/server/controllers/ItemsController.js
@@ -1,26 +1,30 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController";
-
+import { itemsService } from "../services/ItemsService"
 export class ItemsController extends BaseController {
   constructor() {
     super('api/items')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
-      .post('', this.createItems)
-      .delete('/:id', this.deleteItems)
+      .post('', this.create)
+      .delete('/:id', this.delete)
   }
 
-  createItems(req, res, next) {
+  async create(req, res, next) {
     try {
-      throw new Error("Method not implemented.");
+      req.body.creatorId = req.userInfo.id
+      const item = await itemsService.create(req.body)
+      res.send(item)
     } catch (error) {
       next(error)
     }
   }
 
-  deleteItems(req, res, next) {
+  async delete(req, res, next) {
     try {
-      throw new Error("Method not implemented.");
+      const userId = req.userInfo.id
+      const itemId = req.params.id
+      const item = await itemsService.delete(userId, itemId)
     } catch (error) {
       next(error)
     }
--- honey-do/server/services/ItemsService.js
+++ honey-do/server/services/ItemsService.js
@@ -1,4 +1,21 @@
+import { dbContext } from "../db/dbContext"
+import { Forbidden } from "../utils/Errors"
 class ItemsService {
+
+  async create(body) {
+    const item = await dbContext.Items.create(body)
+    await item.populate('creator', 'name id picture')
+    return item
+  }
+
+  async delete(userId, itemId) {
+    const item = await dbContext.Items.findById(itemId)
+    if (item.creatorId.toString() != userId) {
+      throw new Forbidden
+    }
+    const removeItem = await dbContext.Items.findByIdAndDelete(itemId)
+    return removeItem
+  }
 
 }
 export const itemsService = new ItemsService();
