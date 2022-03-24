aa8fb31e33356bcf34e0e6352d607af8e946ce09
Derek <derekbelloni10@gmail.com>
2022-03-24 14:55:21 -0600

    update

---
+++ honey-do/server/controllers/MembersController.js
@@ -0,0 +1,30 @@
+import { membersService } from "../services/MembersService";
+import BaseController from "../utils/BaseController";
+
+export class MembersController extends BaseController {
+  constructor() {
+    super('api/members')
+    this.router
+      .post('', this.create)
+      .delete('', this.remove)
+  }
+
+  async create(req, res, next) {
+    try {
+      req.body.id = req.userInfo.id
+      const member = await membersService.createMember()
+      res.send(member)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async remove(req, res, next) {
+    try {
+      const memberId = req.params.memberId
+      const userId = req.userInfo.id
+      const member = await membersService.removeMember(userId, memberId)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- honey-do/server/services/MembersService.js
+++ honey-do/server/services/MembersService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext"
+import { Forbidden } from "../utils/Errors"
 
 class MembersService {
   async getGroupMembers(id) {
@@ -9,8 +10,13 @@ class MembersService {
   async createMember() {
 
   }
-  async removeMember() {
-
+  async removeMember(userId, memberId) {
+    const foundMember = await dbContext.Members.findById({ id: memberId })
+    const foundGroup = await dbContext.Groups.findById({ groupId: foundMember.groupId })
+    if (foundGroup.creatorId !== userId || foundMember.accountId !== userId) {
+      throw new Forbidden('You are not authorized to remove this member')
+    }
+    await dbContext.Members.findByIdAndDelete(memberId)
   }
 }
 
