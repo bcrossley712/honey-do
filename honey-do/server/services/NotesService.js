082c74a64db6fc65cc3123eb68c0c5d86dc20949
Scott T Lyman <s.t.lyman84@gmail.com>
2022-03-24 13:53:14 -0600

    get group notes update

--- honey-do/server/services/NotesService.js
+++ honey-do/server/services/NotesService.js
@@ -1,6 +1,10 @@
 import { dbContext } from "../db/DbContext"
 
 class NotesService {
+  async getGroupNotes(id) {
+    const notes = await dbContext.Notes.find({ groupId: id })
+    return notes
+  }
   async create(body) {
     const note = dbContext.Notes.create(body)
     return note
