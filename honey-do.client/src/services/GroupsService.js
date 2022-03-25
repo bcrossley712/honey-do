e0591c793990ac49a1716d18ec60fc9d2620493f
Scott T Lyman <s.t.lyman84@gmail.com>
2022-03-25 12:24:01 -0600

    groups create

--- honey-do.client/src/services/GroupsService.js
+++ honey-do.client/src/services/GroupsService.js
@@ -1,4 +1,13 @@
-class GroupsService {
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
+class GroupsService {
+  async createGroup(groupData) {
+    const res = await api.post('api/groups', groupData)
+    logger.log('group created', res.data)
+    AppState.groups.push(res.data)
+    return res.data
+  }
 }
 export const groupsService = new GroupsService()
